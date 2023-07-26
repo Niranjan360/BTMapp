f51202426f4202d6074b24a0e4027482fad3c645
Niranjan360 <connect2hemanthsimha@gmail.com>
2023-07-26 13:59:01 +0530

    movielist component and prop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,40 +1,29 @@
 import { useEffect, useState } from "react";
+import Movielist from "./Movielist";
 
 const Home = () => {    
 
     let[movies , setMovies] = useState(null);
 
     useEffect(()=>{
-        setTimeout(()=>{
-            fetch("http://localhost:4000/movies")
-            .then((res)=>{return res.json() })
-            .then((data)=>{
-                console.log(data.length);
-                setMovies(data);
-            })
-        } , 3000)
+            setTimeout(()=>{
+                fetch("http://localhost:4000/movies")
+                .then((res)=>{return res.json() })
+                .then((data)=>{
+                    console.log(data.length);
+                    setMovies(data);
+                })
+            } , 3000)
     } , [])
 
     return ( 
         <div className="home">
 
-        {movies==null && <h1>Loading ..................</h1>}
+        {movies==null && <div id="loader"></div>}
 
         {movies &&  <>
-                    <h1>All movies</h1>
-                    <div className="movies-list">
-                                    {
-                                        movies.map((m)=>{ return(
-                                            <div className="movie">
-                                                <img src={m.posterurl}/>
-                                                <h1>{m.moviename}</h1>
-                                                <p>IMBD : {m.rating} </p>
-                                                <p>{m.genre}</p>
-                                                <p>{m.languages[0]}</p>
-                                            </div>
-                                        ) })
-                                    }
-                    </div>
+                        <Movielist movies={movies} title="All movie"/>
+                        <Movielist movies={movies} title="Action movie"/>
                     </>}
         </div>
     );
---
+++ src/components/Movielist.jsx
@@ -0,0 +1,23 @@
+const Movielist = ({movies , title}) => {
+
+    
+    return ( 
+        <>
+            <h1>{title}</h1>
+            <div className="movies-list">
+                            {
+                                movies.map((m)=>{ return(
+                                    <div className="movie">
+                                        <img src={m.posterurl}/>
+                                        <h1>{m.moviename}</h1>
+                                        <p>IMBD : {m.rating} </p>
+                                        <p>{m.genre}</p>
+                                        <p>{m.languages[0]}</p>
+                                    </div>
+                                ) })
+                            }
+            </div>
+        </>
+    );
+}
+export default Movielist;
